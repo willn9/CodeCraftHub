8f40be792afe62aaf61045dd944870b11f2961c3
name <wael.nawara@gmail.com>
2024-08-08 15:33:14 -0400

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://root:MjQyMjAtd2FlbG5h@localhost:27017');
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
